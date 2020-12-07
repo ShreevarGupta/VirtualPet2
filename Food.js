6f0ea3539620b05b1baf976a905cc4c9912787bc
ShreevarGupta <71551218+ShreevarGupta@users.noreply.github.com>
2020-12-07 20:03:19 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,29 @@
+class Food {
+
+    constructor() {
+
+        var foodStock, lastFed;
+        
+    }
+
+    getFoodStock() {
+
+
+    }
+
+    updateFoodStock() {
+
+
+    }
+
+    deductFood() {
+
+
+    }
+
+    display() {
+
+        
+    }
+
+}
